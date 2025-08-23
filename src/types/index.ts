2d0df5ce407f42c8d739b94177e53efeefc834e2
Evgeniy Maslov <ruwell.masloves@yandex.ru>
2025-08-23 12:02:45 +0300

    feat: проект 8 разработка документации '0.2'

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,9 +2,13 @@ export interface IProduct {
     id: string;
     title: string;
     category: string;
-    image: string;
     description: string;
-    price: number | null;
+    image: string;
+    price: number;
+}
+
+export interface IProductsCatalog {
+    items: IProduct[];
 }
 
 export interface IContacts {
@@ -14,23 +18,19 @@ export interface IContacts {
     phone: string;
 }
 
-export interface IBasket {
-    items: HTMLElement[];
+export interface IOrder extends IContacts {
+    items: string[];
     total: number;
 }
 
-export interface IOrder extends IContacts {
-    items: string[];
+export interface IBasket {
+    items: HTMLElement[];
     total: number;
 }
 
 export interface IPage {
     products: HTMLElement[];
     counter: number;
-}
-
-export interface IProductsCatalog {
-    items: IProduct[];
 }
 
 export interface ICard {
@@ -41,6 +41,7 @@ export interface ICard {
     image: string;
     price: number;
     index: number;
+    changeButton(price: number, inBasket: boolean): void;
 }
 
 export interface IOrderAnswer {
@@ -53,3 +54,30 @@ export type TContactsForm = Pick<IOrder, 'email' | 'phone'>;
 export type TForm = TPaymentForm & TContactsForm;
 export type FormErrors = Partial<Record<keyof TForm, string>>;
 export type TSuccess = Pick<IBasket, 'total'>;
+
+export interface IWebLarekState {
+	catalog: IProduct[];
+	preview: string | null;
+	order: IOrder;
+	formErrors: FormErrors;
+	setProducts(items: IProduct[]): void;
+	addToBasket(id: string): void;
+	deleteFromBasket(id: string): void;
+	inBasket(id: string): boolean;
+	getBasketProductsCount(): number;
+	getTotal(): number;
+	clearBasket(): void;
+	setOrderField(field: keyof TPaymentForm, value: string): void;
+	setContactsField(field: keyof TContactsForm, value: string): void;
+	validateOrder(): void;
+	validateContacts(): void;
+	setPreview(item: IProduct): void;
+}
+
+export interface ISuccess {
+	total: number;
+}
+
+export interface IModalData {
+	content: HTMLElement;
+}
