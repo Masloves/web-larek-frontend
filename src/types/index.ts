452856aaa7b89c391a578b0b4389b78ffc4cf3ed
Evgeniy Maslov <ruwell.masloves@yandex.ru>
2025-08-25 00:58:41 +0300

    feat: Описал типы данных

--- src/types/index.ts
+++ src/types/index.ts
@@ -11,37 +11,16 @@ export interface IProductsCatalog {
     items: IProduct[];
 }
 
-export interface IContacts {
+export interface IOrderForm {
     payment: string;
     address: string;
     email: string;
     phone: string;
 }
 
-export interface IOrder extends IContacts {
+export interface IOrder extends IOrderForm {
     items: string[];
     total: number;
-}
-
-export interface IBasket {
-    items: HTMLElement[];
-    total: number;
-}
-
-export interface IPage {
-    products: HTMLElement[];
-    counter: number;
-}
-
-export interface ICard {
-    id: string;
-    category: string;
-    title: string;
-    description: string;
-    image: string;
-    price: number;
-    index: number;
-    changeButton(price: number, inBasket: boolean): void;
 }
 
 export interface IOrderAnswer {
@@ -53,31 +32,4 @@ export type TPaymentForm = Pick<IOrder, 'payment' | 'address'>;
 export type TContactsForm = Pick<IOrder, 'email' | 'phone'>;
 export type TForm = TPaymentForm & TContactsForm;
 export type FormErrors = Partial<Record<keyof TForm, string>>;
-export type TSuccess = Pick<IBasket, 'total'>;
-
-export interface IWebLarekState {
-	catalog: IProduct[];
-	preview: string | null;
-	order: IOrder;
-	formErrors: FormErrors;
-	setProducts(items: IProduct[]): void;
-	addToBasket(id: string): void;
-	deleteFromBasket(id: string): void;
-	inBasket(id: string): boolean;
-	getBasketProductsCount(): number;
-	getTotal(): number;
-	clearBasket(): void;
-	setOrderField(field: keyof TPaymentForm, value: string): void;
-	setContactsField(field: keyof TContactsForm, value: string): void;
-	validateOrder(): void;
-	validateContacts(): void;
-	setPreview(item: IProduct): void;
-}
-
-export interface ISuccess {
-	total: number;
-}
-
-export interface IModalData {
-	content: HTMLElement;
-}
+export type ProductCategory = 'софт-скил' | 'другое' | 'дополнительное' | 'кнопка' | 'хард-скил';
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,5 +2,34 @@ export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
 export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
 
 export const settings = {
+	categories: {
+		'софт-скил': 'card__category_soft',
+		другое: 'card__category_other',
+		дополнительное: 'card__category_additional',
+		кнопка: 'card__category_button',
+		'хард-скил': 'card__category_hard',
+	},
 
+	card: {
+		priceless: 'Бесценно',
+	},
+
+	cardButtonValues: {
+		add: 'Купить',
+		delete: 'Удалить из корзины',
+		disabled: 'Недоступно',
+	},
+
+    currency: 'синапсов',
+
+	basket: {
+		null: 'Корзина пуста',
+	},
+
+	formErrors: {
+		payment: 'Необходимо выбрать способ оплаты',
+		address: 'Необходимо указать адрес',
+		email: 'Необходимо указать email',
+		phone: 'Необходимо указать телефон',
+	},
 };
