bb3d6508f6fbfc0ef850a798e2514374b9929a49
Evgeniy Maslov <ruwell.masloves@yandex.ru>
2025-08-25 01:16:34 +0300

    feat: Сделал кнопку в корзине не активной при кустой корзине

--- src/components/common/Basket.ts
+++ src/components/common/Basket.ts
@@ -32,10 +32,12 @@ export class Basket extends Component<IBasketView> {
     set items(items: HTMLElement[]) {
         if (items.length) {
             this._list.replaceChildren(...items);
+            this.setDisabled(this._button, false);
         } else {
             this._list.replaceChildren(createElement<HTMLParagraphElement>('p', {
                 textContent: settings.basket.null,
             }));
+            this.setDisabled(this._button, true);
         }
     }
 
